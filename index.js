182e54d55e49cab32a3ec7e98307b4f9319400f7
Kangmin Han <kangminpersonal@gmail.com>
2021-07-21 00:15:22 +0900

    Server added

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express')
+const fs = require('fs')
+const { request } = require('http')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send("경북대학교 스마트 학생 시스템");
+    fs.readFile('./res/index.html', 'UTF-8', function(err, data) {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        res.end();
+
+
+    });
+    
+
+
 })
 
 app.listen(port, () => {
